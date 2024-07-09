da4cbe7e4a86e9cc6664ae568b82415a1a64f34d
fluffyorang3 <fluffyorang3@gmail.com>
2024-07-09 21:21:56 +0200

    Rework buildTree function

---
+++ balancedTree.js
@@ -0,0 +1,49 @@
+import { merge, mergeSort } from "./mergeSort.js";
+
+function node(data = null, left = null, right = null) {
+  return {
+    data,
+    left,
+    right,
+  };
+}
+
+function tree(array) {
+  array = mergeSort(array);
+  array = [...new Set(array)];
+  let root = buildTree(array, 0, array.length - 1);
+
+  function buildTree(array, start, end) {
+    if (start > end) {
+      return null;
+    }
+    let mid = Math.floor((start + end) / 2);
+    let newNode = node(array[mid]);
+
+    newNode.left = buildTree(array, start, mid - 1);
+    newNode.right = buildTree(array, mid + 1, end);
+    return newNode;
+  }
+  return {
+    root: root,
+  };
+}
+
+const array = [4, 6, 2, 5, 3, 7, 8, 2, 12, 4, 6, 73];
+const bst = tree(array);
+const rooty = bst.root;
+
+const prettyPrint = (rooty, prefix = "", isLeft = true) => {
+  if (rooty === null) {
+    return;
+  }
+  if (rooty.right !== null) {
+    prettyPrint(rooty.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${rooty.data}`);
+  if (rooty.left !== null) {
+    prettyPrint(rooty.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(rooty);
---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let mid = Math.floor(array.length / 2);
+  let left = array.slice(0, mid);
+  let right = array.slice(mid, array.length);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let mergedArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      mergedArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      mergedArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return mergedArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+
+export { merge, mergeSort };
